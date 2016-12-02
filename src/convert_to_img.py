51b0885dcace31eae3788f8187011aa04c6772d3
Michael Mitasev <mitasevm@uw.edu>
2016-12-01 16:54:12 -0800

    small script to convert automap npys to binarize jpg files

---
+++ src/convert_to_img.py
@@ -0,0 +1,14 @@
+import scipy.misc
+import numpy as np
+
+def npy_to_img(fname):
+  npy = np.load('/data/r9k/obs_data/automap/' + fname + '.npy')
+  npy = np.where(npy>200, 1, 0)
+  import pdb; pdb.set_trace()
+  scipy.misc.imsave('/data/r9k/obs_data/automap_img/' + fname +'.jpg', npy[:,:,0])
+
+def batch_convert(list_fname):
+  f = open(list_fname):
+  for line in f:  
+    npy_to_img(line.split('.')[0]
+
